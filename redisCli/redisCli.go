9df83d52f6ec03c29fc26e3797924b27d1d729a1
yangjun <yangjun@dinktalk.com>
2021-11-22 14:06:29 +0800

    update redis cli

--- redisCli/redisCli.go
+++ redisCli/redisCli.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/go-redis/redis/v8"
+	"time"
 )
 
 var ctx = context.Background()
@@ -18,23 +19,23 @@ type redisClient struct {
 
 func InitRedisConfig(host, password string, port, db int) *redisClient {
 	return &redisClient{
-		host: host,
-		port: port,
+		host:     host,
+		port:     port,
 		password: password,
-		db: db,
+		db:       db,
 	}
 }
 
 func (r *redisClient) NewRedisClient() (*redis.Client, error) {
 	var err error
 	r.client = redis.NewClient(&redis.Options{
-		Addr: fmt.Sprintf("%s:%d", r.host, r.port),
+		Addr:     fmt.Sprintf("%s:%d", r.host, r.port),
 		Password: r.password,
-		DB: r.db,
+		DB:       r.db,
 	})
 
 	// 测试连接
-	if err = r.client.Set(ctx, "testConnectKey", 0, 5).Err(); err != nil {
+	if err = r.client.Set(ctx, "testConnectKey", 0, 5 * time.Second).Err(); err != nil {
 		return nil, err
 	}
 	return r.client, nil
